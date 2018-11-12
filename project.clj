8a49418c85750b37cde20d432420bb9ac166beda
Mark Tinsley <mtinsley@greshamtech.com>
2018-11-12 14:25:25 +0000

    up to https://lacinia.readthedocs.io/en/latest/tutorial/game-data.html

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.walmartlabs/lacinia "0.21.0"]]
+
+  :profiles {:dev {:resource-paths ["dev-resources"]}})
